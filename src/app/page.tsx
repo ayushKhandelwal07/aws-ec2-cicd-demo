cc857210cf66bfc1a5d64e7d18df913aca73fbd2
ayushKhandelwal07 <ayushkhandelwal227@gmail.com>
2025-05-30 18:09:36 +0530

    working!

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,8 @@
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      testing the ci cd auto deployment from the github to ec2 instance
-      ci cd pipeline is working lets see the deployment test
-      random change code_test
+      <p>Its working</p>
+      <p>learn ci cd pipeline for deployment of the application on the ec2 instance using github actions and pm2</p>
     </div>
   );
 }
