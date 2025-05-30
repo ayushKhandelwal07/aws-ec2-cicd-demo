3cfbdd54b62a6d5e678fc0244adb89a332ba9ca9
ayushKhandelwal07 <ayushkhandelwal227@gmail.com>
2025-05-30 18:13:43 +0530

    working ?

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <p>Its working</p>
       <p>learn ci cd pipeline for deployment of the application on the ec2 instance using github actions and pm2</p>
+      <p>new wrokign code check</p>
     </div>
   );
 }
